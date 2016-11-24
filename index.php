bf2f662797246adbdc2cb6eda10afc0a1f006ae0
Lars Svendsen <lars@svndsn.dk>
2016-11-24 22:19:31 +0100

    Tilføjet velkomsthilsen på index.php

--- index.php
+++ index.php
@@ -2,13 +2,25 @@
         <div class="container bgcWhite">
             <div class="row">
                 <div class="col-md-6 offset-md-3 text-center">
-                    <h1>Index</h1>  
+                    <h1 id="h1Greeting">Index</h1>
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-6 col-md-offset-3">.col-md-6 .col-md-offset-3</div>
             </div>
-            
-        </div>
- <?php require 'footer.php'; ?>
 
+        </div>
+        <script>
+        $.ajax({
+            method: "GET",
+            url: "http://pba.tese.dk/api/user",
+            data: {
+                id: getCookie("userId")
+            }
+        }).done(function (obj) {
+                console.log(obj);
+                $("#h1Greeting").text("Velkommen " + obj.first_name);
+
+            });
+        </script>
+ <?php require 'footer.php'; ?>
