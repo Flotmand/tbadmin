ad01fd3ec53f152ef28941bfa4d68ecb2b006475
Lars Svendsen <lars@svndsn.dk>
2016-11-25 01:12:07 +0100

    FAQ: Read done

--- faq.php
+++ faq.php
@@ -1,8 +1,8 @@
 <?php require 'header.php'; ?>
-        
+
         <div class="container bgcWhite">
-            <div class="row">
-                <div class="col-md-6">
+            <div class="row" id="newFAQ">
+                <div class="col-md-12">
                     <h3>Tilføj nyt punkt</h3>
                      <form>
                         <div class="form-group">
@@ -18,98 +18,49 @@
                 </div>
                 <br>
                 <br>
-
-                <div class="col-md-6">
-                     <div class="jumbotron">
-                        <h3>INDSATSPERIODER</h3> 
-                        <p>En indsatsperiode er den periode du kan/vil hjælpe Tinderbox.
-                        Du kan vælge i mellem 3 forskellige, og skal minimum vælge en af dem.
-                        Du kan også vælge flere, og på den måde have større chance og flere muligheder for at komme på et team.OBS: På nogle før/efter teams kan det forekomme, at man skal have en vagt før og en vagt efter. Dette vil der dog blive blive gjort opmærksom på ved valg af de/de teams hvor dette kan forekomme.</p> 
-                        <p><strong>Indsatsperioder:</strong>
-                        <br>
-                        Før Tinderbox
-                        <br>
-                        Under Tinderbox
-                        <br>
-                        Efter Tinderbox</p> 
-                        <br>
-                        <button type="button" class="btn btn-lg btn-default">Rediger</button> 
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="clearfix visible-md-block"></div>
-                <div class="col-md-6">
-                    <div class="jumbotron">
-                        <h3>INDSATSTIMER & FORDELING</h3> 
-                        <p>Indsatstimer er det antal timer, som du hjælper Tinderbox.
-                        <br>
-                        <br>
-                        Før Tinderbox = ca. 24 timer
-                        <br>
-                        Under Tinderbox = ca. 16 timer
-                        <br>
-                        Efter Tinderbox = ca. 24 timer</p>
-                        <p>ordelingen af dine indsatstimer pr. vagt defineres af din frivilligkoordinator/teamleder.
-                        Forvent, at dine indsatstimer vil blive fordelt på mere end én sammenhængende vagt, med mindre andet aftales med din frivilligkoordinator/teamleder.</p>
-                        <br>
-                        <button type="button" class="btn btn-lg btn-default">Rediger</button> 
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="jumbotron">
-                        <h3>VAGTBYTTE</h3> 
-                        <p>Har du behov for at bytte en vagt med en af de andre på dit hold, klik på “vagtbytte”. Kontakt din frivilligkoordinator/teamleder vedrørende spørgsmål omkring bytte af din(e) vagt(er).</p> 
-                        <br>
-                        <button type="button" class="btn btn-lg btn-default">Rediger</button> 
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="jumbotron">
-                        <h3>VAGTPLANER</h3> 
-                        <p>Vagtplaner bliver lavet af din frivilligkoordinator/teamleder og lagt ind under din profil i RUBY. Dette bestræber vi os på at gøre ca. medio maj.</p>
-                        <p><strong>OBS!</strong> Du skal, i RUBY, godkende din vagtplan så vi ved at du har set den. Ved mangel på godkendelse forbeholder vi os retten til at afmelde og blackliste dig.</p> 
-                        <br>
-                        <button type="button" class="btn btn-lg btn-default">Rediger</button> 
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="jumbotron">
-                        <h3>ARBEJDSFORSIKRING</h3> 
-                        <p>Alle Frivillige på Tinderbox er forsikret via den lovpligtige arbejdsskadeforsikring. Denne dækker personskade – men ikke private ting. Tager du for eksempel din computer med på Tinderbox, er det dit eget ansvar. Derudover er der en arrangøransvarsforsikring. Den dækker skade på 3. person forvoldt af medarbejder under udførelse af arbejde. Alle skader meddeles omgående til din frivilligkoordinator/teamleder.</p> 
-                        <br>
-                        <button type="button" class="btn btn-lg btn-default">Rediger</button> 
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="jumbotron">
-                        <h3>AUDIO & FOTO</h3> 
-                        <p>Det er ikke tilladt at medbringe båndoptagere, videokamera, spejlreflekskameraer samt professionelt fotoudstyr. Lydfilm og videooptagelse er ikke tilladt. Dog må du gerne medbringe små digitale videokameraer (lommestørrelse) og mobiltelefoner på pladsen uden forudgående tilladelse.
-                        <br>
-                        Det er forbudt at tage billeder af artisterne, bede om en autograf eller på anden måde kontakte dem. Det er vigtigt, at vi alle respekterer artisternes privatliv.</p> 
-                        <br>
-                        <button type="button" class="btn btn-lg btn-default">Rediger</button> 
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="jumbotron">
-                        <h3>CHECK-IN & ARMBÅND</h3> 
-                        <p>Check-In for frivillige på Tinderbox ligger tæt ved hovedindgangen på Falen. Følg skiltene.Vi frigiver din voucher, som er din adgangsbillet til Tinderbox, i starten af juni. Du finder den til download inde i RUBY under din profil. På din voucher/billet findes også et kort, hvor Check-In er markeret.
-                        <br>
-                        <br>
-                        <strong>– OBS:</strong> Mød altid op i god tid – minimum 15 min. før din vagt starter.
-                        <br>
-                        <br>
-                        Dit armbånd giver dig adgang til pladsen i hele perioden.</p> 
-                        <br>
-                        <button type="button" class="btn btn-lg btn-default">Rediger</button> 
-                    </div>
-                </div>
             </div>
         </div>
- 
+    <script>
+        $.ajax({
+            method: "GET",
+            url: "http://pba.tese.dk/api/faq"
+        }).done(function (obj) {
+                console.log(obj);
+
+                var i;
+                var x = 1;
+                var rowIntro;
+                var rowOutro;
+                var out = "";
+                for ( i = 0; i < obj.length; i++) {
+                    //console.log(obj[i].question);
+                      // var x skal hjælpe med at holde styr på hvornår der laves en ny row
+                    if ( x == 1) {
+                      /*rowIntro = '<div class="row">';
+                      rowOutro = "";*/
+
+                      out += '<div class="row">';
+                      //x = 2;
+                    }
+                    /*else {
+                      rowIntro = "";
+                      rowOutro = '</div>';
+                      x = 1;
+                      console.log("Hej hej Else!");
+                    }*/
+                    out += '<div class="col-md-6"><div class="jumbotron"><h3>' + obj[i].question + '</h3><p>' + obj[i].answer + '</p><button type="submit" class="btn btn-default">Rediger</button></div></div>';
+
+                    if ( x == 2 || jQuery.isEmptyObject(obj[i+1]) == true) {
+                      out += '</div>';
+                      x = 1;
+                    }
+                    else {
+                      x = 2;
+                    }
+                }
+
+                $(".container").append(out);
+            });
+        </script>
+
 <?php require 'footer.php'; ?>
